3edbb66a611ac17134ac766236ce795bebc438c0
bdemirel <bdemirel732@gmail.com>
2015-10-08 09:23:00 +0300

    bugfixes and basic login page

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$dsn = 'mysql:host=localhost;dbname=worksheet:charset=utf8';
+$dsn = 'mysql:host=localhost;dbname=worksheet;charset=utf8';
 $db_user = 'root';
 $db_pass = 'root';
 $pdo = new PDO($dsn, $db_user, $db_pass);
--- login.php
+++ login.php
@@ -1,7 +1,23 @@
 <?php
 
-include_once "db_connect.php";
-//include_once "exc_handler.php";
+include_once "template.php";
+session_start();
 
+if (isset($_SESSION['username']))
+{
+
+}
+else
+{
+  $html = "
+<form method='post'>
+  <input type='text' name='username' placeholder='username'>
+  <br>
+  <input type='password' name='password' placeholder='password'>
+  <br>
+  <input type='submit' name='submit' value='Sign In'>
+</form>";
+  template($html);
+}
 
 ?>
