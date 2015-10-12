ae54a5ec5f88ec93eb34cf699813f72fe757f915
bdemirel <bdemirel732@gmail.com>
2015-10-12 14:35:29 +0300

    login page complete

--- db_connect.php
+++ db_connect.php
@@ -3,6 +3,6 @@
 $dsn = 'mysql:host=localhost;dbname=worksheet;charset=utf8';
 $db_user = 'root';
 $db_pass = 'root';
-$pdo = new PDO($dsn, $db_user, $db_pass);
+$dbo = new PDO($dsn, $db_user, $db_pass);
 
 ?>
--- login.php
+++ login.php
@@ -5,19 +5,51 @@ session_start();
 
 if (isset($_SESSION['username']))
 {
-
+  header("location:index.php");
 }
-else
+
+if ($_SERVER['REQUEST_METHOD'] == "POST")
+{
+  foreach($_POST as $name => $data)
+  {
+    ${$name} = $data;
+  }
+  $stmt = $dbo -> prepare("SELECT pword FROM users WHERE uname=:uname");
+  $stmt -> bindParam(':uname', $username);
+  $stmt -> execute();
+  $row = $stmt -> fetch(PDO::FETCH_ASSOC);
+  $hashed = $row['pword'];
+  if (password_verify($password, $hashed))
+  {
+    $_SESSION['username'] = $username;
+    header('location:index.php');
+  }
+  else
+  {
+    $message = 'Problem with username or password';
+    html($message);
+  }
+}
+else if ($_SERVER['REQUEST_METHOD'] == "GET")
+{
+  html();
+}
+else {
+  throw new Exception("Unknown request method!");
+}
+
+function html($message = null)
 {
   $html = "
-<form method='post'>
-  <input type='text' name='username' placeholder='username'>
-  <br>
-  <input type='password' name='password' placeholder='password'>
-  <br>
-  <input type='submit' name='submit' value='Sign In'>
-</form>";
+  <form method='post'>
+    <input type='text' name='username' placeholder='username'>
+    <br>
+    <input type='password' name='password' placeholder='password'>
+    <br>
+    <input type='submit' name='submit' value='Sign In'>
+  </form>
+  $message
+  ";
   template($html);
 }
-
 ?>
